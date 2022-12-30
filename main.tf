699450f4b28e9dd8f9eeafa627c2609ce463ca4a
Mustafa Guler <mustafaguler@hotmail.co.uk>
2022-12-30 21:56:03 +0000

    added vpc and subnet

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
 resource "aws_instance" "my_first_server" {
   ami           = ami-0b5eea76982371e91
   instance_type = "t2.micro"
